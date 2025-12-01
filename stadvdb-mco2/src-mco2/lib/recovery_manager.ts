5db1b25f8a141ec754914fae028fcea4d094ba8a
Your Name <john_ryan_balajadia@dlsu.edu.ph>
2025-12-01 15:00:39 +0800

    failure recovery (no replication yet)

---
+++ stadvdb-mco2/src-mco2/lib/recovery_manager.ts
@@ -0,0 +1,55 @@
+import fs from "fs";
+import path from "path";
+import { TransactionLogEntry } from "./schema";
+import { logFilePath, readNodeLogs } from "./transaction_manager";
+import { redo, undo } from "./recovery_operations";
+import { db0, db1, db2 } from "../db";
+
+const nodePools = {
+  "0": db0,
+  "1": db1,
+  "2": db2,
+};
+
+export async function recoverTransaction(node: number) {
+  const txs: TransactionLogEntry[] = readNodeLogs(node);
+
+  const txMap = new Map<string, TransactionLogEntry[]>();
+  for (const tx of txs) {
+    if (!txMap.has(tx.id)) {
+      txMap.set(tx.id, []);
+    }
+    txMap.get(tx.id)?.push(tx);
+  }
+
+  for (const [key, log] of txMap.entries()) {
+    const committed = log.some(
+      (t) => t.operation == "COMMIT" && t.status !== "COMPLETED"
+    );
+    if (committed) {
+      const operations = log.filter((t) => {
+        return ["INSERT", "UPDATE", "DELETE"].includes(t.operation);
+      });
+      for (const op of operations) {
+        const pool = nodePools[op.node];
+        redo(op, pool);
+        op.status = "COMPLETED";
+      }
+    } else {
+      const operations = log
+        .filter((t) => {
+          return ["INSERT", "UPDATE", "DELETE"].includes(t.operation);
+        })
+        .reverse();
+      for (const op of operations) {
+        const pool = nodePools[op.node];
+        undo(op, pool);
+        op.status = "ABORTED";
+      }
+    }
+  }
+  fs.writeFileSync(
+    path.join(logFilePath, `node${node}_transactions.json`),
+    JSON.stringify(txs)
+  );
+}
---
+++ stadvdb-mco2/src-mco2/lib/recovery_operations.ts
@@ -0,0 +1,122 @@
+import { TransactionLogEntry } from "./schema";
+import { Pool } from "mysql2/promise";
+
+export async function redo(tx: TransactionLogEntry, pool: Pool) {
+  const conn = await pool.getConnection();
+  const tableName = `node${tx.node}_titles`;
+  try {
+    switch (tx.operation) {
+      case "INSERT":
+        await conn.query(
+          `INSERT INTO ${tableName}(tconst, primaryTitle, startYear, runtimeMinutes, genres) VALUES(?, ?, ?, ?, ?)`,
+          [
+            tx.values?.tconst,
+            tx.values?.primaryTitle,
+            tx.values?.startYear,
+            tx.values?.runtimeMinutes,
+            tx.values?.genres,
+          ]
+        );
+        break;
+      case "UPDATE":
+        const columnsToUpdate = [];
+        const values = [];
+        if (tx.values?.primaryTitle !== undefined) {
+          columnsToUpdate.push("primaryTitle = ?");
+          values.push(tx.values.primaryTitle);
+        }
+        if (tx.values?.startYear !== undefined) {
+          columnsToUpdate.push("startYear = ?");
+          values.push(tx.values.startYear);
+        }
+        if (tx.values?.runtimeMinutes !== undefined) {
+          columnsToUpdate.push("runtimeMinutes = ?");
+          values.push(tx.values.runtimeMinutes);
+        }
+        if (tx.values?.genres !== undefined) {
+          columnsToUpdate.push("genres = ?");
+          values.push(tx.values.genres);
+        }
+
+        values.push(tx.values?.tconst);
+
+        await conn.query(
+          `UPDATE ${tableName} SET ${columnsToUpdate.join(
+            ", "
+          )} WHERE tconst = ?`,
+          values
+        );
+        break;
+      case "DELETE":
+        await conn.query(
+          `DELETE FROM ${tableName} WHERE tconst = ?`,
+          tx.values?.tconst
+        );
+        break;
+    }
+  } catch (err) {
+    console.error(`Failed to redo ${tx.operation} level`, err);
+  } finally {
+    if (conn) conn.release();
+  }
+}
+
+export async function undo(tx: TransactionLogEntry, pool: Pool) {
+  const conn = await pool.getConnection();
+  const tableName = `node${tx.node}_titles`;
+  try {
+    switch (tx.operation) {
+      case "INSERT":
+        await conn.query(
+          `DELETE FROM ${tableName} WHERE tconst = ?`,
+          tx.values?.tconst
+        );
+        break;
+      case "UPDATE":
+        const columnsToUpdate = [];
+        const values = [];
+        if (tx.oldValues?.primaryTitle !== undefined) {
+          columnsToUpdate.push("primaryTitle = ?");
+          values.push(tx.oldValues.primaryTitle);
+        }
+        if (tx.oldValues?.startYear !== undefined) {
+          columnsToUpdate.push("startYear = ?");
+          values.push(tx.oldValues.startYear);
+        }
+        if (tx.oldValues?.runtimeMinutes !== undefined) {
+          columnsToUpdate.push("runtimeMinutes = ?");
+          values.push(tx.oldValues.runtimeMinutes);
+        }
+        if (tx.oldValues?.genres !== undefined) {
+          columnsToUpdate.push("genres = ?");
+          values.push(tx.oldValues.genres);
+        }
+
+        values.push(tx.oldValues?.tconst);
+
+        await conn.query(
+          `UPDATE ${tableName} SET ${columnsToUpdate.join(
+            ", "
+          )} WHERE tconst = ?`,
+          values
+        );
+        break;
+      case "DELETE":
+        await conn.query(
+          `INSERT INTO ${tableName}(tconst, primaryTitle, startYear, runtimeMinutes, genres) VALUES(?, ?, ?, ?, ?)`,
+          [
+            tx.oldValues?.tconst,
+            tx.oldValues?.primaryTitle,
+            tx.oldValues?.startYear,
+            tx.oldValues?.runtimeMinutes,
+            tx.oldValues?.genres,
+          ]
+        );
+        break;
+    }
+  } catch (err) {
+    console.error(`Failed to undo ${tx.operation} level`, err);
+  } finally {
+    if (conn) conn.release();
+  }
+}
